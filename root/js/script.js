7c97b30b56f8c93bea448632ecf5b10c306a1ae0
Juan Hamilton <hedwards507@gmail.com>
2020-10-08 16:42:29 -0500

    Fixed nav icon bug

--- root/js/script.js
+++ root/js/script.js
@@ -3,16 +3,11 @@ toggleNav();
 function toggleNav() {
     const linksContainer = document.body.querySelector(".js--links");
     const icon = document.body.querySelector(".js--icon");
-    const urlRoot = `${document.location.origin}/root/images/icon`;
-    const urlImg = `${document.location.origin}/images/icon`;
+    const url = icon.src.slice(0, icon.src.indexOf("icon-"));
 
     icon.addEventListener("click", () => {        
         linksContainer.classList.toggle("hide");
 
-        if (icon.src.includes("root")) {
-            icon.src = icon.src.includes("hamburger") ? `${urlRoot}-close.svg` : `${urlRoot}-hamburger.svg`;
-        } else {
-            icon.src = icon.src.includes("hamburger") ? `${urlImg}-close.svg` : `${urlImg}-hamburger.svg`;
-        }
+        icon.src = icon.src.includes("hamburger") ? `${url}icon-close.svg` : `${url}icon-hamburger.svg`;
     });
 }
